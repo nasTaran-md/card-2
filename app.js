86b071a280546f809427ee1d10dad8e7d2b10c1b
nastaran-md <nastaransarzaeim03@gmail.com>
2023-02-26 12:39:07 +0330

    added app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+
+function plus() {
+    var current = parseInt(document.getElementById('zero').innerHTML);
+    var result = current + 1;
+    document.getElementById('zero').innerHTML = result;
+
+}
+function minus() {
+    var current = parseInt(document.getElementById('zero').innerHTML);
+    var result = current - 1;
+    document.getElementById('zero').innerHTML = result;
+}
